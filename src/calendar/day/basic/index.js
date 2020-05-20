ad4b5c9b3e95c52a0955d6df33fa1721a39f31f9
Harald Pinsker <S1910455003@students.fh-hagenberg.at>
2020-05-20 15:31:23 +0200

    test

--- src/calendar/day/basic/index.js
+++ src/calendar/day/basic/index.js
@@ -92,6 +92,7 @@ class Day extends Component {
         accessibilityRole={isDisabled ? undefined : 'button'}
         accessibilityLabel={this.props.accessibilityLabel}
       >
+	<Text>abc</Text>
         <Text allowFontScaling={false} style={textStyle}>{String(this.props.children)}</Text>
         {dot}
       </TouchableOpacity>
